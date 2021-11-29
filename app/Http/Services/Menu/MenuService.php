5eef723ce60633acfc6e9dbca7820923d36cc57d
nam070202@gmail.com <Nam Nguyen>
2021-11-29 15:34:52 +0700

    first commit

--- app/Http/Services/Menu/MenuService.php
+++ app/Http/Services/Menu/MenuService.php
@@ -1,44 +1,53 @@
-<?php 
+<?php
+
+
 namespace App\Http\Services\Menu;
+
+
 use App\Models\Menu;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Str;                 
+use Illuminate\Support\Str;
 
-
-class MenuService 
+class MenuService
 {
-
     public function getParent()
     {
-        return Menu::where('parent_id',0)->get();
+        return Menu::where('parent_id', 0)->get();
     }
 
+    public function show()
+    {
+        return Menu::select('name', 'id')
+            ->where('parent_id', 0)
+            ->orderbyDesc('id')
+            ->get();
+    }
 
     public function getAll()
     {
         return Menu::orderbyDesc('id')->paginate(20);
     }
+
     public function create($request)
     {
-     try {
-      Menu::create(
-        [
-            'name' => (string)$request-> input('name'),
-            'parent_id' => (string)$request-> input('parent_id'),   
-            'description' => (string)$request-> input('description'),
-            'content' => (string)$request-> input('content'),
-            'active' => (string)$request-> input('active')
-             
-            ]
-      );
-            Session::flash('success', 'Successfully');
-     } catch (\Exception $err) {
+        try {
+            Menu::create([
+                'name' => (string)$request->input('name'),
+                'parent_id' => (int)$request->input('parent_id'),
+                'description' => (string)$request->input('description'),
+                'content' => (string)$request->input('content'),
+                'active' => (string)$request->input('active')
+            ]);
+
+            Session::flash('success', 'Tạo Danh Mục Thành Công');
+        } catch (\Exception $err) {
             Session::flash('error', $err->getMessage());
             return false;
-     }
-     return true;
+        }
+
+        return true;
     }
-    
+
     public function update($request, $menu): bool
     {
         if ($request->input('parent_id') != $menu->id) {
@@ -55,8 +64,6 @@ class MenuService
         return true;
     }
 
-
-
     public function destroy($request)
     {
         $id = (int)$request->input('id');
@@ -69,8 +76,24 @@ class MenuService
     }
 
 
+    public function getId($id)
+    {
+        return Menu::where('id', $id)->where('active', 1)->firstOrFail();
+    }
 
+    public function getProduct($menu, $request)
+    {
+        $query = $menu->products()
+            ->select('id', 'name', 'price', 'price_sale', 'thumb')
+            ->where('active', 1);
 
+        if ($request->input('price')) {
+            $query->orderBy('price', $request->input('price'));
+        }
 
+        return $query
+            ->orderByDesc('id')
+            ->paginate(12)
+            ->withQueryString();
+    }
 }
-
